3d6ce514cdc4f11e45dbb713bb681482347bca2f
obinoemiliano <chaval1986@hotmail.com>
2024-03-21 02:30:00 -0300

    preentrega1

--- src/app.js
+++ src/app.js
@@ -1,48 +1,23 @@
-const ProductManager = require("./src/ProductManager.js");
 const express = require("express");
-const path = require("path");
-const app = express();
-const port = 8080;
+const app = express(); 
+const PUERTO = 8080;
+const productsRouter = require("./routes/products.router.js");
+const cartsRouter = require("./routes/carts.router.js");
 
-const productos = new ProductManager(path.join(__dirname, "./src/ProductManager.json"));
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
-app.get("/products", async (req, res) => {
-  try {
-    let productList = await productos.getProducts();
-    const { limit } = req.query;
+app.get("/", (req, res) => {
+    res.send("Siii funciona");
+})
 
-    if (!limit) {
-      res.json(productList);
-    } else {
-      const limitNumber = parseInt(limit);
-      if (Number.isInteger(limitNumber) && limitNumber > 0) {
-        res.json(productList.slice(0, limitNumber));
-      } else {
-        res.status(400).send(`El límite ${limit} es inválido.`);
-      }
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Error interno del servidor");
-  }
-});
+//Rutas 
+app.use("/api", productsRouter);
+app.use("/api", cartsRouter);
 
-app.get("/products/:id", async (req, res) => {
-  try {
-    const id = parseInt(req.params.id);
-    const product = await productos.getProductById(id);
 
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404).send("Producto no encontrado");
-    }
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Error interno del servidor");
-  }
-});
 
-app.listen(port, () => {
-  console.log(`Aplicación funcionando en el puerto ${port}`);
-});
+app.listen(PUERTO, () => {
+    console.log(`Escuchando en el puerto: ${PUERTO}`);
+})
